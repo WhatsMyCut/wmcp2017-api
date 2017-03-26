35852455a2514a05680379a5f11f4e25e764d726
Mike Taylor <mike@whatsmycut.com>
2017-03-26 18:15:09 -0500

    putting composer files back into  place

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,6 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
@@ -15,5 +14,13 @@ return [
             'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // DB settings
+        db => [
+            'host' => "",
+            'user' => "",
+            'pass' => "",
+            'dbname' => "",
+        ]
     ],
 ];
