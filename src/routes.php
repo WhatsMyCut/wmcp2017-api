d790d1625e6f45097921d50f874efbb2f7889887
Mike Taylor <mike@whatsmycut.com>
2017-04-07 01:01:05 -0500

    debug

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,10 @@
 
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->info("Slim-Skeleton '/" . $args['name'] . " route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+?>
